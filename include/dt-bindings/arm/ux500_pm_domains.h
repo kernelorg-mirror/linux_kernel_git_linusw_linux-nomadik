57e082d4a6ca1c8f90e1d21f03dde898c8b6331f
Linus Walleij <linusw@kernel.org>
2023-01-26 00:07:30 +0100

    dt-bindings: arm: Add the actual power domains on U8500
    This file has been left in an unfinished state just defining
    the root power domain for the U8500 SoC. Fix it up by adding
    the actual existing power domains in this SoC.
    
    The PRCMU code and old regulator driver is mentioning some
    *_RET domains, this means "retention" and is a state in the
    domain and not a domain of its own.
    
    Acked-by: Krzysztof Kozlowski <krzysztof.kozlowski@oss.qualcomm.com>
    Signed-off-by: Linus Walleij <linusw@kernel.org>

--- include/dt-bindings/arm/ux500_pm_domains.h
+++ include/dt-bindings/arm/ux500_pm_domains.h
@@ -8,6 +8,21 @@
 #define _DT_BINDINGS_ARM_UX500_PM_DOMAINS_H
 
 #define DOMAIN_VAPE		0
+#define DOMAIN_VARM		1
+#define DOMAIN_VMODEM		2
+#define DOMAIN_VPLL		3
+#define DOMAIN_VSMPS1		4
+#define DOMAIN_VSMPS2		5
+#define DOMAIN_VSMPS3		6
+#define DOMAIN_VRF1		7
+#define DOMAIN_SVA_MMDSP	8
+#define DOMAIN_SVA_PIPE		9
+#define DOMAIN_SIA_MMDSP	10
+#define DOMAIN_SIA_PIPE		11
+#define DOMAIN_SGA		12
+#define DOMAIN_B2R2_MCDE	13
+#define DOMAIN_ESRAM_12		14
+#define DOMAIN_ESRAM_34		15
 
 
 #endif

3a6c0b6665e4fad9d1b2dce361f048efd1cd0b95
Linus Walleij <linus.walleij@linaro.org>
2023-04-16 15:45:17 +0200

    misc: sram: Generate unique names for subpools
    The current code will, if we do not specify unique labels
    for the SRAM subnodes, fail to register several nodes named
    the same.
    
    Example:
    
    sram@40020000 {
      (...)
      sram@0 {
        (...)
      };
      sram@1000 {
        (...)
      };
    };
    
    Since the child->name in both cases will be "sram" the
    gen_pool_create() will fail because the name is not unique.
    So let's use of_full_node_name() instead of child->name
    so the name is "sram@0" and "sram@1000" respectively.
    
    However if there are two or more SRAMs on the system
    with subnodes named the same (that exists on the U8500)
    then this again will not work. So catenate the top node
    name and the subnode full name to form a string that will
    always be unique.
    
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/misc/sram.c
+++ drivers/misc/sram.c
@@ -240,10 +240,12 @@ static int sram_reserve_regions(struct sram_dev *sram, struct resource *res)
 				goto err_chunks;
 			}
 			if (!label)
-				label = child->name;
-
-			block->label = devm_kstrdup(sram->dev,
-						    label, GFP_KERNEL);
+				block->label = devm_kasprintf(sram->dev, GFP_KERNEL,
+							"%s-%s", dev_name(sram->dev),
+							of_node_full_name(child));
+			else
+				block->label = devm_kstrdup(sram->dev,
+							    label, GFP_KERNEL);
 			if (!block->label) {
 				ret = -ENOMEM;
 				goto err_chunks;

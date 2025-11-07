643e6ae9e34fc3a87c7953b6929265e93b9c38e1
Marek Vasut <marek.vasut+renesas@mailbox.org>
2025-11-08 00:04:26 +0100

    drm/atomic-helper: rcar-du: Enable CRTC early on R-Car DU
    Introduce a variant of drm_atomic_helper_commit_modeset_enables()
    which enables CRTC before encoder/bridge. This is needed on R-Car DU,
    where the CRTC provides clock to LVDS and DSI, and has to be started
    before a bridge may call .prepare , which may trigger e.g. DSI transfer.
    
    This specifically fixes the case where ILI9881C is connected to R-Car
    DU DSI. The ILI9881C panel driver does DSI command transfer in its
    struct drm_panel_funcs .prepare function, which is currently called
    before R-Car DU rcar_du_crtc_atomic_enable() rcar_mipi_dsi_pclk_enable()
    and the DSI command transfer times out.
    
    Fix this by restoring the enable ordering introduced in commit
    c9b1150a68d9 ("drm/atomic-helper: Re-order bridge chain pre-enable
    and post-disable"), to enable CRTC early.
    
    Fixes: c9b1150a68d9 ("drm/atomic-helper: Re-order bridge chain pre-enable and post-disable")
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/gpu/drm/renesas/rcar-du/rcar_du_kms.c
+++ drivers/gpu/drm/renesas/rcar-du/rcar_du_kms.c
@@ -544,7 +544,7 @@ static void rcar_du_atomic_commit_tail(struct drm_atomic_state *old_state)
 	drm_atomic_helper_commit_modeset_disables(dev, old_state);
 	drm_atomic_helper_commit_planes(dev, old_state,
 					DRM_PLANE_COMMIT_ACTIVE_ONLY);
-	drm_atomic_helper_commit_modeset_enables(dev, old_state);
+	drm_atomic_helper_commit_modeset_enables_crtc_early(dev, old_state);
 
 	drm_atomic_helper_commit_hw_done(old_state);
 	drm_atomic_helper_wait_for_flip_done(dev, old_state);

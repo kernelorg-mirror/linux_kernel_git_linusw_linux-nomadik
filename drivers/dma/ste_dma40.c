e396efaac4eb050ffa89f50c30ccf09098d6d477
Linus Walleij <linusw@kernel.org>
2026-09-18 11:23:22 +0200

    dmaengine: ste_dma40: Fix physical cyclic capability
    d40_dmaengine_init() configures dma_both for physical channels that can
    handle both slave and memcpy transfers. Physical DMA40 channel setup has
    supported cyclic LLIs since cyclic transfer support was added, but the
    DMA_CYCLIC capability is set on dma_slave a second time instead of
    dma_both.
    
    Set DMA_CYCLIC on dma_both so d40_ops_init() installs
    device_prep_dma_cyclic and physical channels advertise cyclic support.
    
    Fixes: 0c842b551063 ("dma40: cyclic xfer support")
    Assisted-by: LLM
    Signed-off-by: Linus Walleij <linusw@kernel.org>

--- drivers/dma/ste_dma40.c
+++ drivers/dma/ste_dma40.c
@@ -2897,7 +2897,7 @@ static int __init d40_dmaengine_init(struct d40_base *base,
 	dma_cap_zero(base->dma_both.cap_mask);
 	dma_cap_set(DMA_SLAVE, base->dma_both.cap_mask);
 	dma_cap_set(DMA_MEMCPY, base->dma_both.cap_mask);
-	dma_cap_set(DMA_CYCLIC, base->dma_slave.cap_mask);
+	dma_cap_set(DMA_CYCLIC, base->dma_both.cap_mask);
 
 	d40_ops_init(base, &base->dma_both);
 	err = dmaenginem_async_device_register(&base->dma_both);
